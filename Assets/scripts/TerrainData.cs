59dba6790bd299bf3a233a3e438228e5792ef98c
[Umar59] <240503umar@gmail.com>
2021-04-14 00:01:44 +0300

    made terrain spawner, nothin else!

---
+++ Assets/scripts/TerrainData.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Terrain Data", menuName = "Terrain Data")]
+public class TerrainData : ScriptableObject
+{
+    public GameObject terrain;
+    public int maxChunksAtTime;
+                //max chunks of particular typa terrain can be spawned in a row
+}
---
+++ Assets/scripts/terrainSpawner.cs
@@ -0,0 +1,56 @@
+﻿using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class terrainSpawner : MonoBehaviour
+{
+
+    [SerializeField] private int maxChunks;
+    [SerializeField] private List<TerrainData> terrainDatas = new List<TerrainData>();
+
+    private Vector3 currentPosition = new Vector3(0f, 0f, 0f);
+    private List<GameObject> currentTerrain = new List<GameObject>();
+
+    void Start()
+    {
+        for (int i = 0; i < maxChunks; i++)
+        {
+            TerrainSpawn();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            TerrainSpawn();
+
+        }
+
+    }
+
+    private void TerrainSpawn()
+    {
+
+        int whichTerrainToSpawn = Random.Range(0, terrainDatas.Count);
+        int terrainMaxInRow = Random.Range(1, terrainDatas[whichTerrainToSpawn].maxChunksAtTime);
+        for (int i = 0; i< terrainMaxInRow; i++)
+        {
+            GameObject terrains = Instantiate(terrainDatas[whichTerrainToSpawn].terrain, currentPosition, Quaternion.identity);
+
+            currentTerrain.Add(terrains);
+            if (currentTerrain.Count > maxChunks)
+            {
+
+                Destroy(currentTerrain[0]);
+                currentTerrain.RemoveAt(0);
+            }
+            currentPosition.z += 2;
+
+
+        }
+     
+    }
+}
